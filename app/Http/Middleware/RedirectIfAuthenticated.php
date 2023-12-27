85177669978a986088b5eafa0b2dfda3ceff987b
kanakokos <piano.music0408@gmail.com>
2023-12-27 20:11:36 +0900

    タイムアウト後、ログイン画面に遷移するように修正

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -22,11 +22,6 @@ class RedirectIfAuthenticated
             return redirect(RouteServiceProvider::HOME);
         }
 
-        // タイムアウトした場合の処理を追加
-        if (Auth::guard($guard)->guest()) {
-            return redirect()->route('login');
-        }
-
         return $next($request);
     }
 }
