ec402052ee824d5fe47c6bbff3da0ec998c7d313
kanakokos <piano.music0408@gmail.com>
2024-01-30 21:50:11 +0900

    ユーザー検索、ユーザー画面のレイアウト修正

--- public/js/user_search.js
+++ public/js/user_search.js
@@ -7,3 +7,20 @@ $(function () {
     $('.subject_inner').slideToggle();
   });
 });
+
+function toggleSubjects() {
+  const arrowIcon = document.querySelector('.subject_edit_btn');
+  const subjectInner = document.querySelector('.subject_inner');
+
+  subjectInner.classList.toggle('show');
+  arrowIcon.textContent = subjectInner.classList.contains('show') ? '選択科目の編集 ▲' : '選択科目の編集 ▼';
+}
+
+
+function toggleSearchConditions() {
+  const conditionsInner = document.querySelector('.search_conditions_inner');
+  const icon = document.querySelector('.toggle-icon .icon');
+
+  conditionsInner.classList.toggle('show');
+  icon.textContent = conditionsInner.classList.contains('show') ? '▲' : '▼';
+}
--- resources/views/authenticated/users/search.blade.php
+++ resources/views/authenticated/users/search.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.sidebar')
 
 @section('content')
-<p>ユーザー検索</p>
-<div class="search_content w-100 border d-flex">
+<div class="search_content w-100 d-flex">
   <div class="reserve_users_area">
     @foreach($users as $user)
-    <div class="border one_person blockquote">
+    <div class="border one_person blockquote" style="margin:10px; padding:10px;">
       <div>
         <span>ID : </span><span>{{ $user->id }}</span>
       </div>
@@ -54,36 +53,40 @@
     </div>
     @endforeach
   </div>
-  <div class="search_area w-25 border">
+  <div class="search_area w-25">
     <div class="">
+      <h4 style="color:#6C6C78;">検索</h4>
       <div>
         <input type="text" class="free_word" name="keyword" placeholder="キーワードを検索" form="userSearchRequest">
       </div>
       <div>
-        <lavel>カテゴリ</lavel>
+        <lavel style="color:#6C6C78;">カテゴリ</lavel>
         <select form="userSearchRequest" name="category">
           <option value="name">名前</option>
           <option value="id">社員ID</option>
         </select>
       </div>
       <div>
-        <label>並び替え</label>
+        <label style="color:#6C6C78;">並び替え</label>
         <select name="updown" form="userSearchRequest">
           <option value="ASC">昇順</option>
           <option value="DESC">降順</option>
         </select>
       </div>
       <div class="">
-        <p class="m-0 search_conditions"><span>検索条件の追加</span></p>
+        <p class="m-0 search_conditions toggle-icon" onclick="toggleSearchConditions()">
+          <span style="color:#6C6C78;">検索条件の追加</span>
+          <span class="icon" style="color:#6C6C78;">▼</span>
+        </p>
         <div class="search_conditions_inner">
           <div>
-            <label>性別</label>
+            <label style="color:#6C6C78;">性別</label><br>
             <span>男</span><input type="radio" name="sex" value="1" form="userSearchRequest">
             <span>女</span><input type="radio" name="sex" value="2" form="userSearchRequest">
             <span>その他</span><input type="radio" name="sex" value="3" form="userSearchRequest">
           </div>
           <div>
-            <label>権限</label>
+            <label style="margin-top:20px" style="color:#6C6C78;">権限</label>
             <select name="role" form="userSearchRequest" class="engineer">
               <option selected disabled>----</option>
               <option value="1">教師(国語)</option>
@@ -93,7 +96,7 @@
             </select>
           </div>
           <div class="selected_engineer">
-            <label>選択科目</label>
+            <label style="color:#6C6C78;">選択科目</label>
             @foreach($subjects as $subject)
               <div class="">
               <input type="checkbox" name="subject[]" value="{{ $subject->id }}" form="userSearchRequest">
@@ -104,10 +107,10 @@
         </div>
       </div>
       <div>
-        <input type="reset" value="リセット" form="userSearchRequest">
+        <input type="submit" name="search_btn" value="検索" form="userSearchRequest">
       </div>
       <div>
-        <input type="submit" name="search_btn" value="検索" form="userSearchRequest">
+        <input type="reset" value="リセット" form="userSearchRequest">
       </div>
     </div>
     <form action="{{ route('user.show') }}" method="get" id="userSearchRequest"></form>
